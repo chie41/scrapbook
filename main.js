b69e5e23755de14a3611b8784349486337aaf74a
chie41 <91408610+chie41@users.noreply.github.com>
2025-03-08 21:44:22 +0700

    test

---
+++ main.js
@@ -0,0 +1,70 @@
+// References to DOM Elements
+const leftPage = document.querySelector("#left-page");
+const rightPage = document.querySelector("#right-page");
+const book = document.querySelector("#book");
+
+const paper1 = document.querySelector("#p1");
+const paper2 = document.querySelector("#p2");
+const paper3 = document.querySelector("#p3");
+
+// Business Logic
+let currentLocation = 1;
+let numOfPapers = 3;
+let maxLocation = numOfPapers + 1;
+
+function openBook() {
+    book.style.transform = "translateX(50%)";
+}
+
+function closeBook(isAtBeginning) {
+    if (isAtBeginning) {
+        book.style.transform = "translateX(0%)";
+    } else {
+        book.style.transform = "translateX(100%)";
+    }
+}
+
+function goNextPage() {
+    if (currentLocation < maxLocation) {
+        switch (currentLocation) {
+            case 1:
+                openBook();
+                paper1.classList.add("flipped");
+                break;
+            case 2:
+                paper2.classList.add("flipped");
+                break;
+            case 3:
+                paper3.classList.add("flipped");
+                closeBook(false);
+                break;
+        }
+        currentLocation++;
+    }
+}
+
+function goPrevPage() {
+    if (currentLocation > 1) {
+        switch (currentLocation) {
+            case 2:
+                closeBook(true);
+                paper1.classList.remove("flipped");
+                paper1.style.zIndex = 3; // Đưa trang này lên trên cùng
+                break;
+            case 3:
+                paper2.classList.remove("flipped");
+                paper2.style.zIndex = 2;
+                break;
+            case 4:
+                openBook();
+                paper3.classList.remove("flipped");
+                paper3.style.zIndex = 1;
+                break;
+        }
+        currentLocation--;
+    }
+}
+
+// Add Event Listeners
+rightPage.addEventListener("click", goNextPage);
+leftPage.addEventListener("click", goPrevPage);
